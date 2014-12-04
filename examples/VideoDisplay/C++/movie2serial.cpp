fafe79157ccf8581d6ed344ac2165aa3558c7212
schwarrrtz <davidphillipschwartz@gmail.com>
2014-12-04 02:41:50 +0000

    python is faster on the Macbook but c++ is MUCH faster on the RasPi

--- examples/VideoDisplay/C++/movie2serial.cpp
+++ examples/VideoDisplay/C++/movie2serial.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <pthread.h>
 #include <stdlib.h>
 #include <iostream>
 #include <string>
@@ -8,6 +9,8 @@
 // dimensions of the video & the LEDs
 static int width = 300;
 static int height = 8;
+
+static float max_framerate = 30.0;
 
 namespace b_io = boost::asio;
 namespace b_sys = boost::system;
@@ -20,7 +23,7 @@ int main(int argc, char* argv[])
     b_io::serial_port port(ioService);
     try
     {
-        port.open("/dev/tty.usbmodem408061");
+        port.open("/dev/ttyACM0");
         port.set_option(b_io::serial_port_base::baud_rate(115200));
     }
     catch(b_sys::system_error& e)
@@ -57,21 +60,29 @@ int main(int argc, char* argv[])
     { 
         // fill data frame from ffmpeg pipe
         size_t bytesRead = 0;
-        while(bytesRead < 3*width*height)
+        while((bytesRead < 3*width*height) && !feof(ffmpegPipe))
         {
             bytesRead += fread((outBuf + bytesRead + 3), sizeof(char), 3*width*height - bytesRead, ffmpegPipe);
         }
     
         // synchronous write
-        b_io::write(port, b_io::buffer(outBuf, 3*(width*height + 1)));
-
-        // calculate framerate
-        if(((frameCount + 1) % 10) == 0)
+        if(bytesRead == 3*width*height)
         {
-            oldTime = newTime;
-            newTime = b_time::microsec_clock::local_time();
-            b_time::time_duration delta = newTime - oldTime;
-            std::cout << "framerate: " << 10000.0 / delta.total_milliseconds() << std::endl;
+            b_io::write(port, b_io::buffer(outBuf, 3*(width*height + 1)));
+        }
+
+        // calculate framerate & throttle if necessary        
+        oldTime = newTime;
+        newTime = b_time::microsec_clock::local_time();
+        b_time::time_duration delta = newTime - oldTime;
+        int delay = 1000000/max_framerate - 1000*delta.total_milliseconds();
+        if(delay > 0)
+        {
+            usleep(delay);
+        }    
+        else if(((frameCount + 1) % 10) == 0)
+        {
+            std::cout << "framerate: " << ((delay > 0) ? 30 : delta.total_milliseconds() / 1000) << std::endl;
         }
         
         frameCount++;
