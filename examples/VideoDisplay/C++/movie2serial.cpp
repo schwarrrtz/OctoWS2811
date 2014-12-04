cc53a1a3cbcadd1e7ece2965ecdcc8b06c0be699
schwarrrtz <davidphillipschwartz@gmail.com>
2014-12-03 17:37:50 -0800

    added movie2serial.cpp to VideoDisplay

---
+++ examples/VideoDisplay/C++/movie2serial.cpp
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <boost/asio.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+// dimensions of the video & the LEDs
+static int width = 300;
+static int height = 8;
+
+namespace b_io = boost::asio;
+namespace b_sys = boost::system;
+namespace b_time = boost::posix_time;
+
+int main(int argc, char* argv[])
+{
+    // initialize serial port
+    b_io::io_service ioService;
+    b_io::serial_port port(ioService);
+    try
+    {
+        port.open("/dev/tty.usbmodem408061");
+        port.set_option(b_io::serial_port_base::baud_rate(115200));
+    }
+    catch(b_sys::system_error& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+    
+    // ask videoDisplay for format information
+    char query = '?';
+    b_io::write(port, b_io::buffer(&query, sizeof(query)));
+    b_io::streambuf inBuf;
+    b_io::read_until(port, inBuf, '\n');
+    std::istream inStream(&inBuf);
+    std::string formatData;
+    std::getline(inStream, formatData);
+    std::cout << "format data: " << formatData << std::endl;
+    
+    // initialize ffmpeg
+    FILE* ffmpegPipe = popen("ffmpeg -loglevel warning -f concat -i loop.txt -f image2pipe -pix_fmt rgb24 -vcodec rawvideo -", "r");
+    if(!ffmpegPipe)
+    {
+        std::cerr << "failed to open ffmpeg process" << std::endl;
+        return 1;
+    }
+    
+    // write video data
+    b_time::ptime oldTime;
+    b_time::ptime newTime = b_time::microsec_clock::local_time();
+    int frameCount = 0;    
+    char* outBuf = (char*)malloc(3*(width*height + 1)*sizeof(char));
+    outBuf[0] = '*'; outBuf[1] = '0'; outBuf[2] = '0';
+    while(!feof(ffmpegPipe))
+    { 
+        // fill data frame from ffmpeg pipe
+        size_t bytesRead = 0;
+        while(bytesRead < 3*width*height)
+        {
+            bytesRead += fread((outBuf + bytesRead + 3), sizeof(char), 3*width*height - bytesRead, ffmpegPipe);
+        }
+    
+        // synchronous write
+        b_io::write(port, b_io::buffer(outBuf, 3*(width*height + 1)));
+
+        // calculate framerate
+        if(((frameCount + 1) % 10) == 0)
+        {
+            oldTime = newTime;
+            newTime = b_time::microsec_clock::local_time();
+            b_time::time_duration delta = newTime - oldTime;
+            std::cout << "framerate: " << 10000.0 / delta.total_milliseconds() << std::endl;
+        }
+        
+        frameCount++;
+    }
+    
+    free(outBuf);
+    std::cout << "read " << frameCount << " frames successfully" << std::endl;
+
+    // finalize
+    port.close();                
+    return pclose(ffmpegPipe);
+}
